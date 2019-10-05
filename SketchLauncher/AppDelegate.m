301ee38b4394bba1e68d863b47afa38342055d91
Marek Hrušovský <xhruso00@gmail.com>
2019-10-05 13:19:13 +0200

    Removed static strings

--- SketchLauncher/AppDelegate.m
+++ SketchLauncher/AppDelegate.m
@@ -1,54 +1,38 @@
 #import "AppDelegate.h"
 
-static NSString *const kSketchBundleIdentifier = @"com.bohemiancoding.sketch3";
+static NSString *const kAppToLaunchBundleIdentifier = @"com.bohemiancoding.sketch3";
 
 @implementation AppDelegate
 
-- (void)dealloc
-{
-    [super dealloc];
-}
-
--(void)bringToFrontApplicationWithBundleIdentifier:(NSString*)inBundleIdentifier
-{
-    // Try to bring the application to front
-    NSArray* appsArray = [NSRunningApplication runningApplicationsWithBundleIdentifier:inBundleIdentifier];
-    if([appsArray count] > 0)
-    {
-        [[appsArray objectAtIndex:0] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
-    }
-    
-    // Quit ourself
-    [[NSApplication sharedApplication] terminate:self];
-}
-
--(void)launchApplicationWithPath:(NSString*)inPath andBundleIdentifier:(NSString*)inBundleIdentifier
-{
-    if(inPath != nil)
-    {
-        // Run Sketch.app and inject our dynamic library
-        NSString *dyldLibrary = [[NSBundle bundleForClass:[self class]] pathForResource:@"SketchOverrides" ofType:@"dylib"];
-        NSString *launcherString = [NSString stringWithFormat:@"DYLD_INSERT_LIBRARIES=\"%@\" \"%@\" &", dyldLibrary, inPath];
-        
-        system([launcherString UTF8String]);
-        
-        // Bring it to front after a delay
-        dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC));
-        dispatch_after(delay, dispatch_get_main_queue(), ^{
-            [self bringToFrontApplicationWithBundleIdentifier:inBundleIdentifier];
-        });
-    }
-}
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSString *bundleIdentifier = kSketchBundleIdentifier;
-    NSURL *appURL = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:bundleIdentifier];
+    NSURL *appURL = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:kAppToLaunchBundleIdentifier];
     NSBundle *bundle = [NSBundle bundleWithURL:appURL];
     NSString *executablePath = [bundle executablePath];
-    if([[NSFileManager defaultManager] fileExistsAtPath:executablePath]) {
-        [self launchApplicationWithPath:executablePath andBundleIdentifier:bundleIdentifier];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:executablePath] == NO) {
+        NSLog(@"++++++ NO EXECUTABLE FOUND: %@ ++++", executablePath);
+        return;
     }
+    NSString *dyldLibrary = [[[NSBundle mainBundle] pathsForResourcesOfType:@"dylib" inDirectory:nil] firstObject];
+    if (dyldLibrary) {
+        [self launchApplicationWithInjectionPath:dyldLibrary];
+    }
+}
+
+-(void)launchApplicationWithInjectionPath:(NSString *)dyldLibraryPath
+{
+    // Run xxx.app and inject our dynamic library
+    NSError *error;
+    NSURL *appURL = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:kAppToLaunchBundleIdentifier];
+    NSDictionary *env = @{@"DYLD_INSERT_LIBRARIES" : dyldLibraryPath};
+    NSRunningApplication *runningApplication = [[NSWorkspace sharedWorkspace] launchApplicationAtURL:appURL options:NSWorkspaceLaunchAsync configuration:@{NSWorkspaceLaunchConfigurationEnvironment : env} error:&error];
+    
+    // Bring it to front after a delay
+    dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC));
+    dispatch_after(delay, dispatch_get_main_queue(), ^{
+        [runningApplication activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+        [[NSApplication sharedApplication] terminate:self];
+    });
 }
 
 @end
